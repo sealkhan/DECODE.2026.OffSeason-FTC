bec8710ef4762580ef12201b09317c1854d80f88
Detective-Anna <146679668+Detective-Anna@users.noreply.github.com>
2024-10-28 16:49:18 -0500

    changed build gradle in team code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto Blue Close", group = "LinearOpMode")
+//Declares as autonomous file, SDK thing
+public class Auto extends Hardware {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initHardware();
+        stopMoving();
+
+        //
+    }
+}
