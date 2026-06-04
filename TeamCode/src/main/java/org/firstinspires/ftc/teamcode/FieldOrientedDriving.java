52718fd6d2735d6520502ec25d3c11c3ed7b1574
sealkhan <khan.batulzainab@gmail.com>
2026-06-04 14:28:59 -0500

    Update FieldOrientedDriving.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldOrientedDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldOrientedDriving.java
@@ -199,9 +199,9 @@ public class FieldOrientedDriving extends Hardware {
 
             // Gamepad x kicks in the third ball
             if(gamepad2.x){
-                thirdBallKick.setPosition(0.45);
-            } else {
                 thirdBallKick.setPosition(0.0);
+            } else {
+                thirdBallKick.setPosition(0.7);
             }
 
             // Gamepad a opens the door
